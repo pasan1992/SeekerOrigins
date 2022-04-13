5c7a36b28c2a0a4412d560fdd29688ee38392c29
Pasan Nadeera Udawatta <pnudawatta@gmail.com>
2022-04-13 06:32:31 +0530

    Example Level 2

---
+++ Assets/GameAssets/Scripts/NavAgentChangeTask.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavAgentChangeTask : MonoBehaviour
+{
+    public void ChangeAreaMask(string MaskName,NavMeshAgent agent)
+    {
+        int areaMask = agent.areaMask;
+        areaMask = 1 << NavMesh.GetAreaFromName("Nothing");//turn on all
+        areaMask += 1 << NavMesh.GetAreaFromName(MaskName);
+        //areaMask += 1 << NavMesh.GetAreaFromName(MaskName);
+        // areaMask |= (1 << LayerMask.NameToLayer("Nothing"));
+        // areaMask |= (1 << LayerMask.NameToLayer(MaskName));
+        agent.areaMask=areaMask;
+    }
+}
