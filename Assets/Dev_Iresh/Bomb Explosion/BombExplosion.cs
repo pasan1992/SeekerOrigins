b350c8f6d70073848b32ec0116c97654dbdb9f12
Iresh Sampath <ireshsampath@gmail.com>
2022-05-22 14:04:37 +0530

    create bomb Explosion

---
+++ Assets/Dev_Iresh/Bomb Explosion/BombExplosion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class BombExplosion : MonoBehaviour
+{
+    [SerializeField] CameraShake _cameraShake;
+    [SerializeField] VisualEffect _visualEffect;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _visualEffect.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _visualEffect.Play();
+        _cameraShake.StartExplosion();
+    }
+
+    public void StartExplosion(){
+        _cameraShake.StartExplosion();
+    }
+}
