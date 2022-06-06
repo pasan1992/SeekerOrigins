96b6a24670572cb805f9028a032e6b0feed0b94e
Iresh Sampath <ireshsampath@gmail.com>
2022-06-06 08:43:48 +0530

    bugs fixed

--- Assets/GameAssets/Scripts/UI/UIManager.cs
+++ Assets/GameAssets/Scripts/UI/UIManager.cs
@@ -21,10 +21,14 @@ public class UIManager : MonoBehaviour
     [SerializeField] public Image _rifleImg;
     [SerializeField] public Image _missileImg;
     [SerializeField] public Image _grenadeImg;
+    [SerializeField] public Image _injectionImg;
     [SerializeField] public TMP_Text _pistolCountTxt;
     [SerializeField] public TMP_Text _rifleCountTxt;
     [SerializeField] public TMP_Text _missileCountTxt;
     [SerializeField] public TMP_Text _grenedCountTxt;
+    [SerializeField] public TMP_Text _injectionCountTxt;
+
+    [SerializeField] public TMP_Text _gameOverTxt;
 
     [SerializeField] public Image _healthBarImg;
     [SerializeField] public Image _regenBarImg;
@@ -48,8 +52,8 @@ public class UIManager : MonoBehaviour
     public AgentData _agentData;
 
     //UI Animation variable
-    float _healthTweenTime = 1f;
-    float _sheildTweenTime = 1f;
+    float _healthTweenTime = 0.5f;
+    float _sheildTweenTime = 0.5f;
 
     float _prevHealth = 1;
     float _prevSheild = 1;
@@ -68,6 +72,7 @@ public class UIManager : MonoBehaviour
         m_movingAgent.setOnAmmoPickupCallback(OnAmmoPickupEvent);
         m_movingAgent.setOnHealCallback(onHealthChange);
 
+        
         update_health();
     }
     // Update is called once per frame
@@ -136,6 +141,7 @@ public class UIManager : MonoBehaviour
 
         _pickupMsgTxtQueue.Add(msgLine);
     }
+
     void Update()
     {
         updateLootText();
@@ -145,6 +151,51 @@ public class UIManager : MonoBehaviour
 
     void updateAmmo()
     {
+        //if (m_movingAgent.getCurrentWeapon() == null)
+        //{
+        //    _activeWeapon.SetActive(false);
+        //}
+        //else
+        //{
+        //    _activeWeapon.SetActive(true);
+        //}
+
+        if (m_movingAgent.getCurrentWeapon() != null)
+        {
+            _bulletCountTxt.text = m_movingAgent.getCurrentWeaponAmmoCount().ToString() + " / " + m_movingAgent.getCurrentWeaponMagazineSize().ToString();
+            _bulletsBarImg.fillAmount = ((float)m_movingAgent.getCurrentWeaponAmmoCount()) / ((float)m_movingAgent.getCurrentWeaponMagazineSize());
+
+            //_magazinCountTxt.text = (m_movingAgent.getCurrentWeaponMagazineSize() * m_movingAgent.getCurrentWeaponAmmoCount()).ToString(); //TODO
+
+            //var weaponType = m_movingAgent.getCurrentWeaponType(); 
+
+            //if (weaponType.ToString() == "primary")
+            //{
+            //    _weaponImg.sprite = _weaponList[0];
+            //}
+            //else if (weaponType.ToString() == "secondary")
+            //{
+            //    _weaponImg.sprite = _weaponList[1];
+            //}
+        }
+        else
+        {
+            _bulletCountTxt.text = "0";
+            _bulletsBarImg.fillAmount = 0;
+        }
+
+        // Set Injection Count
+        _injectionCountTxt.text = m_movingAgent.GetAgentData().HealInjectionCount.ToString();
+        if (m_movingAgent.GetAgentData().HealInjectionCount <= 0)
+        {
+            _injectionImg.color = Color.black;
+        }
+        else
+        {
+            _injectionImg.color = Color.white;
+        }
+
+        // Set Missile Count
         int missile_count = m_movingAgent.GetAgentData().checkAvailableAmmo("Missile");
         _missileCountTxt.text = missile_count.ToString();
 
@@ -157,16 +208,7 @@ public class UIManager : MonoBehaviour
             _missileImg.color = Color.white;
         }
 
-
-        if (m_movingAgent.getCurrentWeapon() == null)
-        {
-            _activeWeapon.SetActive(false);
-        }
-        else
-        {
-            _activeWeapon.SetActive(true);
-        }
-
+        // Set Grened Count
         _grenedCountTxt.text = m_movingAgent.GetGrenadeCount().ToString();
 
         if (m_movingAgent.GetGrenadeCount() <= 0)
@@ -178,7 +220,17 @@ public class UIManager : MonoBehaviour
             _grenadeImg.color = Color.white;
         }
 
-        if(m_movingAgent.GetPrimaryWeapon() == null)
+        // Set Primary Weapon Count
+        if (m_movingAgent.GetPrimaryWeapon() != null)
+        {
+            var val = m_movingAgent.GetAgentData().checkAvailableAmmo(m_movingAgent.GetPrimaryWeapon().m_weaponAmmunitionName);
+            _rifleCountTxt.text = val.ToString();
+        }
+        else
+        {
+            _rifleCountTxt.text = "0";
+        }
+        if (m_movingAgent.GetPrimaryWeapon() == null)
         {
             _rifleImg.color = Color.black;
         }
@@ -189,6 +241,17 @@ public class UIManager : MonoBehaviour
         else
         {
             _rifleImg.color = Color.white;
+        }
+
+        // Set Secondary Weapon Count
+        if (m_movingAgent.GetSecondaryWeapon() != null)
+        {
+            var val = m_movingAgent.GetAgentData().checkAvailableAmmo(m_movingAgent.GetSecondaryWeapon().m_weaponAmmunitionName);
+            _pistolCountTxt.text = val.ToString();
+        }
+        else
+        {
+            _pistolCountTxt.text = "0";
         }
 
         if (m_movingAgent.GetSecondaryWeapon() == null)
@@ -203,56 +266,35 @@ public class UIManager : MonoBehaviour
         {
             _pistolImg.color = Color.white;
         }
-
-        if (m_movingAgent.getCurrentWeapon() != null)
-        {
-            _bulletCountTxt.text = m_movingAgent.getCurrentWeaponAmmoCount().ToString() + " / " + m_movingAgent.getCurrentWeaponMagazineSize().ToString();
-            _bulletsBarImg.fillAmount = ((float)m_movingAgent.getCurrentWeaponAmmoCount()) / ((float)m_movingAgent.getCurrentWeaponMagazineSize());
-
-            //_magazinCountTxt.text = (m_movingAgent.getCurrentWeaponMagazineSize() * m_movingAgent.getCurrentWeaponAmmoCount()).ToString(); //TODO
-
-            var weaponType = m_movingAgent.getCurrentWeaponType(); 
-
-            if (weaponType.ToString() == "primary")
-            {
-                _weaponImg.sprite = _weaponList[0];
-            }
-            else if (weaponType.ToString() == "secondary")
-            {
-                _weaponImg.sprite = _weaponList[1];
-            }
-        }
-        else
-        {
-            _bulletCountTxt.text = "0";
-            _bulletsBarImg.fillAmount = 0;
-        }
     }
 
     private void update_health()
     {
-        // if (_agentData.Sheild > 0)
-        // {
-        //     _havingSheald = true;
-        //     _sheildCountTxt.text = ((int)_agentData.Sheild).ToString();
-        //     AnimSheild();
-        // }
-        // else if(_agentData.Sheild == 0 && _havingSheald)
-        // {
-        //     _sheildCountTxt.text = ((int)_agentData.Sheild).ToString();
-        //     AnimSheild();
-        //     _havingSheald = false;
-        // }
-        // else
-        // {
-        //     _healthCountTxt.text = ((int)_agentData.Health).ToString();
-        //     AnimHealth();
-        // }
-
+         if (_agentData.Sheild > 0)
+         {
+             _havingSheald = true;
+             _sheildCountTxt.text = ((int)_agentData.Sheild).ToString();
+             AnimSheild();
+         }
+         else if(_agentData.Sheild == 0 && _havingSheald)
+         {
+             _sheildCountTxt.text = ((int)_agentData.Sheild).ToString();
+             AnimSheild();
+             _havingSheald = false;
+         }
+         else
+         {
+             _healthCountTxt.text = ((int)_agentData.Health).ToString();
+             AnimHealth();
+         }
+        if (_agentData.Health <= 0)
+        {
+            _gameOverTxt.gameObject.SetActive(true);
+        }
         _sheildCountTxt.text = ((int)_agentData.Sheild).ToString();
         _healthCountTxt.text = ((int)_agentData.Health).ToString();
-        AnimHealth();
-        AnimSheild();
+        //AnimHealth();
+        //AnimSheild();
     }
 
     public void AnimHealth()
@@ -262,22 +304,57 @@ public class UIManager : MonoBehaviour
         LeanTween.scale(_healthCountTxt.gameObject, new Vector3(0.7f, 0.7f, 0.7f) * 2f, 2)
             .setEasePunch();
 
-
-
         //LeanTween.value(_healthCountTxt.gameObject, 0, 1, 3)
         //    .setEasePunch()
         //    .setOnUpdate(setHealthTxt);
 
-
         var currentHealth = _agentData.Health / _agentData.MaxHealth;
 
-        LeanTween.value(_healthBarImg.gameObject, currentHealth, _prevHealth, _healthTweenTime)
+        if (_prevHealth > currentHealth)
+        {
+            LeanTween.value(_healthBarImg.gameObject, _prevHealth, currentHealth, _healthTweenTime)
             .setEaseInElastic()
             .setOnUpdate((value) =>
             {
                 _healthBarImg.fillAmount = currentHealth == 0 ? 0 : value;
                 _prevHealth = currentHealth;
             });
+        }
+        else if (_prevHealth <= currentHealth)
+        {
+            LeanTween.value(_healthBarImg.gameObject, _prevHealth, currentHealth, _healthTweenTime)
+            .setEaseInElastic()
+            .setOnUpdate((value) =>
+            {
+                _healthBarImg.fillAmount = currentHealth == 0 ? 0 : value;
+                _prevHealth = currentHealth;
+            });
+        }
+
+        //if (_prevHealth > currentHealth)
+        //{
+        //    LeanTween.value(_healthBarImg.gameObject, currentHealth, _prevHealth, _healthTweenTime)
+        //    .setEaseInElastic()
+        //    .setOnUpdate((value) =>
+        //    {
+        //        _healthBarImg.fillAmount = currentHealth == 0 ? 0 : value;
+        //        _prevHealth = currentHealth;
+        //    });
+        //}
+        //else if(_prevHealth <= currentHealth)
+        //{
+        //    LeanTween.value(_healthBarImg.gameObject, _prevHealth, currentHealth, _healthTweenTime)
+        //    .setEaseInElastic()
+        //    .setOnUpdate((value) =>
+        //    {
+        //        _healthBarImg.fillAmount = currentHealth == 0 ? 0 : value;
+        //        _prevHealth = currentHealth;
+        //    });
+        //}
+        //.setEasePunch()
+
+        //_healthBarImg.fillAmount = currentHealth == 1 ? 1 : value;
+
     }
 
     public void AnimSheild()
@@ -289,13 +366,34 @@ public class UIManager : MonoBehaviour
 
         var currentSheild = _agentData.Sheild / _agentData.MaxSheild;
 
-        LeanTween.value(_regenBarImg.gameObject, currentSheild, _prevSheild, _sheildTweenTime)
-            .setEaseInOutElastic()
+        //LeanTween.value(_regenBarImg.gameObject, currentSheild, _prevSheild, _sheildTweenTime)
+        //    .setEaseInOutElastic()
+        //    .setOnUpdate((value) =>
+        //    {
+        //        _regenBarImg.fillAmount = currentSheild == 0 ? 0 : value;
+        //        _prevSheild = currentSheild;
+        //    });
+
+        if (_prevSheild > currentSheild)
+        {
+            LeanTween.value(_regenBarImg.gameObject, _prevSheild, currentSheild, _sheildTweenTime)
+            .setEaseInElastic()
             .setOnUpdate((value) =>
             {
                 _regenBarImg.fillAmount = currentSheild == 0 ? 0 : value;
                 _prevSheild = currentSheild;
             });
+        }
+        else if (_prevSheild <= currentSheild)
+        {
+            LeanTween.value(_regenBarImg.gameObject, _prevSheild, currentSheild, _sheildTweenTime)
+            .setEaseInElastic()
+            .setOnUpdate((value) =>
+            {
+                _regenBarImg.fillAmount = currentSheild == 0 ? 0 : value;
+                _prevSheild = currentSheild;
+            });
+        }
     }
 
     private void update_ammo_count()
