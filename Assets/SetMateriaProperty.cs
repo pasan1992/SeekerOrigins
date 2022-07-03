e0b3a6d7b62267a2a9ca2027defcc241c347646d
Pasan Nadeera Udawatta <pnudawatta@gmail.com>
2022-07-03 13:58:43 +0530

    test

---
+++ Assets/SetMateriaProperty.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetMateriaProperty : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void SetMaterialEmmision(bool enabled, Material mat)
+    {
+        if(enabled)
+        {
+            mat.EnableKeyword("_EMISSION");
+        }
+        else{
+            mat.DisableKeyword("_EMISSION");
+        }
+        
+    }
+}
