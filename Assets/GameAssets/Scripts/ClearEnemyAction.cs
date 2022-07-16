a2b121f81b0b110d158eb7931871281943b5ab58
Pasan Nadeera Udawatta <pnudawatta@gmail.com>
2022-07-16 15:28:35 +0530

    temp

---
+++ Assets/GameAssets/Scripts/ClearEnemyAction.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace HutongGames.PlayMaker.Actions
+{
+
+public class ClearEnemyAction : FsmStateAction
+{
+    private DamagableObject[] m_enemyAgents;
+    private int destroyedEnemyCount = 0;
+    public GameObject EnemySet;
+    public FsmEvent finishEvent;
+    public override void OnEnter()
+    {
+        m_enemyAgents = EnemySet.GetComponentsInChildren<DamagableObject>();
+        destroyedEnemyCount = 0;
+        foreach (DamagableObject agent in m_enemyAgents)
+        {
+            agent.setOnDestroyed(onUnitDestory);
+            var agent_cont = agent.getTransfrom().GetComponent<AgentController>();
+            if(agent_cont!=null)
+            {
+                agent_cont.RemoveRestrictions();
+            }
+        }
+    }
+
+    public void onUnitDestory()
+    {
+        destroyedEnemyCount += 1;
+        if(destroyedEnemyCount == m_enemyAgents.Length)
+        {
+            Fsm.Event(finishEvent);
+            Finish();
+        }
+    }
+
+    
+    IEnumerator waitAndAttack(AgentController agent,Transform target)
+    {
+        yield return new WaitForSeconds(2);
+        agent.ForceCombatMode(target);
+    }
+
+    #if UNITY_EDITOR
+
+    public override float GetProgress()
+    {
+        return ((float)destroyedEnemyCount)/m_enemyAgents.Length;
+    }
+
+#endif
+}
+
+}
+
