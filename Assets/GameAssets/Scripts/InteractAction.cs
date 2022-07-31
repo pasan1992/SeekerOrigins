91178649efdc1a5070ebec76a07cf7bd4d722520
Pasan Nadeera Udawatta <pnudawatta@gmail.com>
2022-08-01 04:15:14 +0530

    test

--- Assets/GameAssets/Scripts/InteractAction.cs
+++ Assets/GameAssets/Scripts/InteractAction.cs
@@ -41,8 +41,9 @@ public class InteractionAction : FsmStateAction
         {
             //Fsm.Event(finishEvent);
             finished = true;
-            Finish();
             m_interactable.SetInteratable(false);
+            Finish();
+            
         }
     }
 
