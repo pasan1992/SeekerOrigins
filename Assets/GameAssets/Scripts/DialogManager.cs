3953d0c7e10d357442fd9af6fadaafe837c4f844
Pasan Nadeera Udawatta <pnudawatta@gmail.com>
2022-11-27 21:13:35 +0530

    dialog character

--- Assets/GameAssets/Scripts/DialogManager.cs
+++ Assets/GameAssets/Scripts/DialogManager.cs
@@ -14,6 +14,8 @@ public class DialogManager : MonoBehaviour
         public AudioClip audio;
         public string charName;
         public float time;
+
+        public DialogManager.Characters character;
     }
 
     // Start is called before the first frame update
@@ -26,9 +28,17 @@ public class DialogManager : MonoBehaviour
     public GameEvents.BasicNotifactionEvent onDialogEndCallback;
     public TMP_Text DialogText;
     public TMP_Text ObjectiveText;
+
+    public Image CharacterImage;
+
     public GameObject dialogUI;
 
     private bool pause = false;
+
+    public enum Characters {Alex_afraid,Alex_angry,Alex_happy,Alex_normal,Govnor_Happy,Govnor_Disapointed,Govnor_Normal,Govnor_Angry
+                            ,Kangarian_Angry,Kangarian_Normal,Fang_normal};
+    public Dictionary<DialogManager.Characters,Sprite> imageDict;
+
     void Awake()
     {
         if(instance == null)
@@ -38,6 +48,53 @@ public class DialogManager : MonoBehaviour
         else if (instance != this)
         {
             Destroy(gameObject);
+        }
+        initalizeImages();
+    }
+
+    private void initalizeImages()
+    {
+        imageDict = new Dictionary<DialogManager.Characters,Sprite>();
+        foreach (Characters type in System.Enum.GetValues(typeof(Characters)))
+        {   
+            string rp_path = "";
+            switch(type)
+            {
+                case Characters.Fang_normal:
+                rp_path = "Images/Fang_normal";
+                break;
+                case Characters.Kangarian_Angry:
+                rp_path = "Images/Kangarian_Angry";
+                break;
+                case Characters.Kangarian_Normal:
+                rp_path = "Images/Kangarian_Normal";
+                break;
+                case Characters.Alex_afraid:
+                rp_path = "Images/Alex_afraid";
+                break;
+                case Characters.Alex_angry:
+                rp_path = "Images/Alex_angry";
+                break;
+                case Characters.Alex_happy:
+                rp_path = "Images/Alex_happy";
+                break;
+                case Characters.Alex_normal:
+                rp_path = "Images/Alex_normal";
+                break;
+                case Characters.Govnor_Disapointed:
+                rp_path = "Images/Govnor_disapointed";
+                break;
+                case Characters.Govnor_Happy:
+                rp_path = "Images/Govnor_happy";
+                break;
+                case Characters.Govnor_Normal:
+                rp_path = "Images/Govnor_normal";
+                break;
+                case Characters.Govnor_Angry:
+                rp_path = "Images/Gavnor_angry";
+                break;
+            }
+            imageDict.Add(type,Resources.Load<Sprite>(rp_path));
         }
     }
     void Start()
@@ -121,6 +178,8 @@ public class DialogManager : MonoBehaviour
         m_audioSource.Stop();
         m_audioSource.PlayOneShot(statement.audio);
         DialogText.text = statement.charName + " : " + statement.text + " (ENTER:SKIP)";
+        CharacterImage.sprite = imageDict[statement.character];
+
     }
 
     public int getRemaningLines()
