8f6922f6627ccc22eaf577a476492189832ab859
Iresh Sampath <ireshsampath@gmail.com>
2023-03-06 18:36:40 +0530

    update dialog UI M3

---
+++ Assets/GameAssets/Scripts/UI/UIFloatingDialog.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFloatingDialog : MonoBehaviour
+{
+    [SerializeField] GameObject _mainCam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.rotation = Quaternion.LookRotation(_mainCam.transform.position);
+        transform.rotation = Quaternion.LookRotation(new Vector3(35,-25,0));
+    }
+}
